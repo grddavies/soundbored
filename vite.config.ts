b53ab8336d41afce016c01ae79dd3c0bbf1510d4
Gethin Davies <grd.davies@gmail.com>
2023-01-13 02:03:23 +0000

    :earth_africa: Change vite base for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcssNesting from 'postcss-nesting';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-soundboard/', // GH-pages location
   plugins: [react()],
 });
