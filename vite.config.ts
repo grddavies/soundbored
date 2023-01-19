8dc650ca110cf3fc818c9c1d348ad37cd379556c
Gethin Davies <grd.davies@gmail.com>
2023-01-19 15:19:58 +0000

    Update deploy location

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import solidPlugin from 'vite-plugin-solid';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-soundboard/', // GH-pages location
+  base: '/solid-soundboard/', // GH-pages location
   plugins: [solidPlugin()],
 });
