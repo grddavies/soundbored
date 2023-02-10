e6db26d67dabf8ca1441e35d758b3dc2ef1593db
Gethin Davies <gethin.davies@motilent.co.uk>
2023-02-10 18:07:16 +0000

    Fix useObservable hook return types

--- src/hooks/useObservable.ts
+++ src/hooks/useObservable.ts
@@ -1,15 +1,15 @@
-import { onMount, createSignal, onCleanup, Accessor } from 'solid-js';
+import { onMount, createSignal, onCleanup, Accessor, Setter } from 'solid-js';
 import { Observable, ObservableReadonly } from 'src/utils';
 
 export function useObservable<T>(
   observable: Observable<T>,
-): [Accessor<T>, (value: T) => void];
+): [Accessor<T>, Setter<T>];
 export function useObservable<T>(
   observable: ObservableReadonly<T>,
 ): [Accessor<T>];
 export function useObservable<T>(
   observable: Observable<T> | ObservableReadonly<T>,
-): [Accessor<T>, (value: T) => void] | [T] {
+): [Accessor<T>, (v: T) => void] | [Accessor<T>] {
   const [signal, setSignal] = createSignal(observable.value);
   const updateFunc = (v: T) => {
     setSignal(() => v);
