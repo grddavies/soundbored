fabbfc850e04d0dc168edb2ec28bbb5999e211cb
Gethin Davies <grd.davies@gmail.com>
2023-02-17 01:52:43 +0000

    Move beep out of default 12

--- src/defaults/Defaults.ts
+++ src/defaults/Defaults.ts
@@ -12,7 +12,6 @@ export class Defaults {
     new DirtSample('moog/000_Mighty Moog C2.wav', 'Moog C2'),
     new DirtSample('juno/03_juno_chorus_low.wav', 'Juno'),
     new DirtSample('jazz/007_SN.wav', 'Snare'),
-    new DirtSample('bleep/pc_beep.wav', 'Beep'),
     new DirtSample('bleep/checkpoint-hit.wav', 'Bleep'),
     new DirtSample('808/CH.WAV', '808CH'),
     new DirtSample('808oh/OH00.WAV', '808OH'),
@@ -22,6 +21,7 @@ export class Defaults {
     new DirtSample('808/RS.WAV', '808RS'),
     new DirtSample('juno/04_juno_chorus_mid.wav', 'Juno Mid'),
     new DirtSample('birds/001_10.wav', 'Birdies'),
+    new DirtSample('bleep/pc_beep.wav', 'Beep'),
   ] as const;
 }
 
